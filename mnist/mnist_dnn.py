a9abb1ca86259f5c93813dcda966627103fb1747
lizonghang <870644199@qq.com>
2017-07-27 16:07:11 +0800

    dnn mnist

--- mnist/mnist_dnn.py
+++ mnist/mnist_dnn.py
@@ -9,8 +9,9 @@ class MnistNet(object):
         self.mnist = input_data.read_data_sets('handwrite_datasets', one_hot=True)
         self._build_net()
         self.sess = tf.Session()
-        self.sess.run(tf.global_variables_initializer())
+        tf.summary.merge_all()
         tf.summary.FileWriter("logs", self.sess.graph)
+        self.sess.run(tf.global_variables_initializer())
 
     def _build_net(self):
         self.x = tf.placeholder(tf.float32, [None, 784], name='x')
@@ -18,29 +19,28 @@ class MnistNet(object):
             self.W1 = tf.Variable(tf.random_uniform([784, 10], -0.05, 0.05, name='W1'))
             self.b1 = tf.Variable(tf.constant(0.1, shape=[1, 10]), name='b1')
             self.l1 = tf.matmul(self.x, self.W1) + self.b1
+            tf.summary.histogram('layer_1/W1', self.W1)
+            tf.summary.histogram('layer_1/b1', self.b1)
         with tf.name_scope('softmax'):
             self.y = tf.nn.softmax(self.l1, name='output')
         with tf.name_scope('train'):
             self.y_ = tf.placeholder(tf.float32, [None, 10])
             self.cross_entropy = -tf.reduce_sum(self.y_ * tf.log(self.y + 0.01))
             self.train_op = tf.train.GradientDescentOptimizer(self.lr).minimize(self.cross_entropy)
+            tf.summary.scalar('loss', self.cross_entropy)
         with tf.name_scope('eval'):
             self.correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_, 1))
             self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+            tf.summary.scalar('accuracy', self.accuracy)
 
 
 if __name__ == '__main__':
-    accuracy_list = []
-    for round in range(10):
-        net = MnistNet()
-        for i in range(30000):
-            batch_xs, batch_ys = net.mnist.train.next_batch(100)
-            net.sess.run(net.train_op, {net.x: batch_xs, net.y_: batch_ys})
-            if i % 1000 == 0:
-                print 'step {0}, accuracy = {1}'.format(
-                    i,
-                    net.sess.run(net.accuracy, feed_dict={net.x: net.mnist.test.images, net.y_: net.mnist.test.labels})
-                )
-        accuracy_list.append(net.sess.run(net.accuracy, feed_dict={net.x: net.mnist.test.images, net.y_: net.mnist.test.labels}))
-        del net
-    print 'average accurracy: ', sum(accuracy_list) / float(len(accuracy_list))
+    net = MnistNet()
+    for i in range(30000):
+        batch_xs, batch_ys = net.mnist.train.next_batch(100)
+        net.sess.run(net.train_op, {net.x: batch_xs, net.y_: batch_ys})
+        if i % 1000 == 0:
+            print 'step {0}, accuracy = {1}'.format(
+                i,
+                net.sess.run(net.accuracy, feed_dict={net.x: net.mnist.test.images, net.y_: net.mnist.test.labels})
+            )
