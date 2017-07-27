51604fefd4e33bbdc14aaaaf6bc48f2701afdc53
lizonghang <870644199@qq.com>
2017-07-27 18:41:07 +0800

    mnist cnn

--- mnist/mnist_cnn.py
+++ mnist/mnist_cnn.py
@@ -98,12 +98,12 @@ writer = tf.summary.FileWriter('logs', graph=sess.graph)
 for i in range(10000):
     batch = mnist.train.next_batch(50)
     sess.run(train_op, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    # if i % 100 == 0:
+    #     print 'step {0}, training accuracy {1}'.format(
+    #         i,
+    #         sess.run(accuracy, feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+    #     )
     if i % 100 == 0:
-        print 'step {0}, training accuracy {1}'.format(
-            i,
-            sess.run(accuracy, feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-        )
-    if i % 1000 == 0:
         summary = sess.run(tf.summary.merge_all(), feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
         writer.add_summary(summary, i)
 
