a2354396ca5aa94558525a95dfbc8fd72b5be6b7
lizonghang <870644199@qq.com>
2017-08-01 11:25:32 +0800

    RL MountainCar

---
+++ RL_MountainCar/RL_brain.py
@@ -0,0 +1,109 @@
+# coding=utf-8
+import numpy as np
+import tensorflow as tf
+import sys
+
+np.random.seed(1)
+tf.set_random_seed(1)
+
+
+class PolicyGradient:
+    def __init__(
+            self,
+            n_actions,
+            n_features,
+            learning_rate=0.01,
+            reward_decay=0.95,
+            output_graph=False,
+    ):
+        self.n_actions = n_actions
+        self.n_features = n_features
+        self.lr = learning_rate
+        self.gamma = reward_decay
+
+        self.experience_observations = []
+        self.experience_actions = []
+        self.experience_rewards = []
+
+        self._build_net()
+        self.sess = tf.Session()
+        if output_graph: tf.summary.FileWriter("logs/", self.sess.graph)
+        self.sess.run(tf.global_variables_initializer())
+
+    def _build_net(self):
+        with tf.name_scope('inputs'):
+            self.tf_observation = tf.placeholder(tf.float32, [None, self.n_features], name="observations")
+            self.tf_actions = tf.placeholder(tf.int32, [None, ], name="actions")
+            self.tf_rewards = tf.placeholder(tf.float32, [None, ], name="rewards")
+
+        # full-connect-layer-1
+        layer_1 = tf.layers.dense(
+            inputs=self.tf_observation,
+            units=10,
+            activation=tf.nn.tanh,
+            kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.3),
+            bias_initializer=tf.constant_initializer(0.1),
+            name='fc1'
+        )
+
+        # full-connect-layer-2
+        all_act = tf.layers.dense(
+            inputs=layer_1,
+            units=self.n_actions,
+            activation=None,
+            kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.3),
+            bias_initializer=tf.constant_initializer(0.1),
+            name='fc2'
+        )
+
+        # softmax-output
+        self.all_act_prob = tf.nn.softmax(all_act, name='action_probability')
+
+        with tf.name_scope('loss'):
+            # to maximize total reward (log_p * R) is to minimize -(log_p * R), and the tf only have minimize(loss)
+            # neg_log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=all_act, labels=self.tf_acts)
+            # or in this way:
+            neg_log_prob = tf.reduce_sum(-tf.log(self.all_act_prob)*tf.one_hot(self.tf_actions, self.n_actions), axis=1)
+            loss = tf.reduce_mean(neg_log_prob * self.tf_rewards)
+
+        with tf.name_scope('train'):
+            self.train_op = tf.train.AdamOptimizer(self.lr).minimize(loss)
+
+    def choose_action(self, observation):
+        prob_weights = self.sess.run(self.all_act_prob, feed_dict={self.tf_observation: observation[np.newaxis, :]})
+        action = np.random.choice(range(prob_weights.shape[1]), p=prob_weights.ravel())
+        return action
+
+    def store_transition(self, s, a, r):
+        self.experience_observations.append(s)
+        self.experience_actions.append(a)
+        self.experience_rewards.append(r)
+
+    def learn(self):
+        discounted_experience_rewards_norm = self._discount_and_norm_rewards()
+
+        self.sess.run(self.train_op, feed_dict={
+             self.tf_observation: np.vstack(self.experience_observations),  # shape=[None, n_obs]
+             self.tf_actions: np.array(self.experience_actions),  # shape=[None, ]
+             self.tf_rewards: discounted_experience_rewards_norm,  # shape=[None, ]
+        })
+
+        self.experience_observations, self.experience_actions, self.experience_rewards = [], [], []
+
+        return discounted_experience_rewards_norm
+
+    def _discount_and_norm_rewards(self):
+        # discount episode rewards
+        discounted_experience_rewards = np.zeros_like(self.experience_rewards)
+        running_add = 0
+        for t in reversed(range(0, len(self.experience_rewards))):
+            running_add = running_add * self.gamma + self.experience_rewards[t]
+            discounted_experience_rewards[t] = running_add
+
+        # normalize episode rewards
+        discounted_experience_rewards -= np.mean(discounted_experience_rewards)
+        discounted_experience_rewards /= np.std(discounted_experience_rewards)
+        return discounted_experience_rewards
+
+
+
